30b426030631b489307e730f606387174d41e0cf
Albert Wu <albert12132@gmail.com>
2015-01-24 15:59:47 -0800

    Fail gracefully on imported syntaxerrors. Fixes #6

--- client/sources/doctest/__init__.py
+++ client/sources/doctest/__init__.py
@@ -3,6 +3,7 @@ from client.sources.common import importing
 from client.sources.doctest import models
 import logging
 import os
+import traceback
 
 log = logging.getLogger(__name__)
 
@@ -20,7 +21,23 @@ def load(file, name, args):
         log.info('Cannot import doctests from {}'.format(file))
         raise ex.LoadingException('Cannot import doctests from {}'.format(file))
 
-    module = importing.load_module(file)
+    try:
+        module = importing.load_module(file)
+    except Exception:
+        # Assume that part of the traceback includes frames from importlib.
+        # Begin printing the traceback after the last line involving importlib.
+        # TODO(albert): Try to find a cleaner way to do this. Also, might want
+        # to move this to a more general place.
+        print('Traceback (most recent call last):')
+        stacktrace = traceback.format_exc().split('\n')
+        start = 0
+        for i, line in enumerate(stacktrace):
+            if 'importlib' in line:
+                start = i + 1
+        print('\n'.join(stacktrace[start:]))
+
+        raise ex.LoadingException('Error importing file {}'.format(file))
+
     if not hasattr(module, name):
         raise ex.LoadingException('Module {} has no function {}'.format(
                                   module.__name__, name))
