ca40f4e335a13d0d06fb60fdb05ee1e4126b2e6a
Albert Wu <albert12132@gmail.com>
2015-04-05 19:13:30 -0700

    Add demo for sqlite usage

---
+++ demo/sqlite/tests/q1.py
@@ -0,0 +1,32 @@
+test = {
+  'name': 'Question 1',
+  'points': 2,
+  'suites': [
+    {
+      'type': 'sqlite',
+      'setup': r"""
+      sqlite> .read hw1.sql
+      """,
+      'cases': [
+        {
+          'code': r"""
+          sqlite> select * from colors
+          red|primary
+          blue|primary
+          green|secondary
+          yellow|primary
+          """,
+        },
+        {
+          'code': r"""
+          sqlite> select color from colors
+          red
+          blue
+          green
+          yellow
+          """,
+        },
+      ],
+    }
+  ]
+}
