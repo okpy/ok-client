ffb1f80cbbd7537c13ffdeda3d07e9c5c141d52d
Albert Wu <albert12132@gmail.com>
2015-07-01 10:30:32 -0700

    Make ok-test and ok share the same command line arguments

--- client/cli/test.py
+++ client/cli/test.py
@@ -1,4 +1,5 @@
 from client import exceptions as ex
+from client.cli import ok
 from client.cli.common import assignment
 from client.cli.common import messages
 from client.protocols import grading
@@ -14,34 +15,9 @@ log = logging.getLogger('client')   # Get top-level logger
 
 CLIENT_ROOT = os.path.dirname(client.__file__)
 
-def parse_input():
-    """Parses command line input."""
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-q', '--question', type=str, action='append',
-                        help="focus on specific questions")
-    parser.add_argument('-u', '--unlock', action='store_true',
-                        help="unlock tests interactively")
-    parser.add_argument('-i', '--interactive', action='store_true',
-                        help="toggle interactive mode")
-    parser.add_argument('-c', '--config', type=str,
-                        help="Specify a configuration file")
-    parser.add_argument('-s', '--score', action='store_true',
-                        help="Score the assignment")
-    parser.add_argument('--timeout', type=int, default=10,
-                        help="Specify a timeout limit")
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help="Specify verbose mode")
-    parser.add_argument('--debug', action='store_true',
-                        help="show debug statements")
-    parser.add_argument('--export', action='store_true',
-                        help="This is a nop. Used for compatibility with OK.")
-    return parser.parse_args()
-
 def main():
     """Run the LockingProtocol."""
-    args = parse_input()
+    args = ok.parse_input()
 
     log.setLevel(logging.DEBUG if args.debug else logging.ERROR)
     log.debug(args)
