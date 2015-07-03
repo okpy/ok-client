77f6694e7811f351cd50f6b8975cd85c4f9a10ee
Albert Wu <albert12132@gmail.com>
2015-07-03 01:23:06 -0700

    Modify grading success message

--- client/utils/format.py
+++ client/utils/format.py
@@ -61,7 +61,7 @@ def print_progress_bar(header, passed, failed, locked, verbose=True):
               'first failed test case'.format(passed))
         return
     else:
-        print('    All {} test cases passed!'.format(passed))
+        print('    {} test cases passed! No cases failed.'.format(passed))
         return
 
     # Print [oook.....] progress bar
