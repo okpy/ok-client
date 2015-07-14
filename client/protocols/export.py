88b4c2c62341d6228a70bc71c712826c907080b6
Albert Wu <albert12132@gmail.com>
2015-07-13 17:53:52 -0700

    Fix export --latest bug

--- client/protocols/export.py
+++ client/protocols/export.py
@@ -17,37 +17,37 @@ class ExportProtocol(models.Protocol):
     """Downloads submissions from the server"""
 
     def run(self, messages):
-        """If the --export parameter was used, downloads all submissions 
+        """If the --export parameter was used, downloads all submissions
         for the current assignment from the server and then exits
         """
         if not self.args.export:
             return
-        
+
         self.access_token = auth.authenticate(self.args.authenticate)
         log.info('Authenticated with access token %s', self.access_token)
-        
+
         data = None
         try:
             data = pickle.load(open(EXPORT_CACHE, "rb"))
         except IOError:
             data = {
-                'endpoint': self.assignment.endpoint   
+                'endpoint': self.assignment.endpoint
             }
             data['assign'], data['course'] = self.get_ids(data['endpoint'])
             data['students'] = self.get_students(data['course'])
             if not data['students']:
                 return
             data['current'] = 0
-        
+
         current_student = 0
         try:
-            if not os.path.exists(SUBMISSION_DIR): 
+            if not os.path.exists(SUBMISSION_DIR):
                 os.makedirs(SUBMISSION_DIR)
-            
+
             downloads_left = len(data['students']) - data['current']
-            
+
             print("{0} submissions left to download.".format(downloads_left))
-            
+
             for current_student in range(data['current'], len(data['students'])):
                 student = data['students'][current_student]
                 try:
@@ -57,16 +57,16 @@ class ExportProtocol(models.Protocol):
                     abort(data, len(data['students']), current_student)
                     return
                 data['current'] = current_student + 1
-                    
+
         except KeyboardInterrupt:
             pickle.dump(data, open("export_cache.pkl", "wb"))
             abort(data, len(data['students']), current_student)
             return
-            
+
         print("Submissions downloaded.")
         if os.path.exists(EXPORT_CACHE):
             os.remove(EXPORT_CACHE)
-        
+
     def download_submission(self, student, assign_id):
         """Downloads a student's final submission for an assignment"""
         if self.args.latest:
@@ -91,7 +91,7 @@ class ExportProtocol(models.Protocol):
                 .format(student, subm_id, timestamp))
             f.flush()
             os.fsync(f.fileno())
-            
+
     def get_final_submission(self, student, assign_id):
         """Gets the final_submission from the server for use in download_submission"""
         params = {'assignment': assign_id}
@@ -105,11 +105,11 @@ class ExportProtocol(models.Protocol):
         timestamp = datetime.strptime(raw_data['submission']['backup']['server_time'], GAE_DATETIME_FORMAT)
         subm_id = raw_data['submission']['backup']['id']
         return contents, timestamp, subm_id
-        
+
     def get_latest_submission(self, student, assign_id):
         """Gets the latest submission from the server for use in download_submission"""
         params = {'assignment': assign_id}
-        data = self.request('user/{0}/get_submissions'.format(student[0]), 
+        data = self.request('user/{0}/get_submissions'.format(student[0]),
             params)['data']
         if len(data) == 0:
             print("No submissions for {0}".format(student[0]))
@@ -121,21 +121,21 @@ class ExportProtocol(models.Protocol):
             if newest_time is None or time > newest_time:
                 newest_time = time
                 newest_subm = subm
-        contents = newest_subm['messages']['file_contents']
+        contents = newest_subm['backup']['messages']['file_contents']
         return contents, newest_time, newest_subm['id']
-       
+
     def get_ids(self, endpoint):
         """Gets the course and assignment id for the given endpoint"""
         response = self.request('assignment')
         for assign in response['data']['results']:
             if assign['name'] == endpoint:
                 return assign['id'], assign['course']['id']
-        
+
     def request(self, route, params={}):
         """Makes an API request"""
-        return network.api_request(self.access_token, self.args.server, 
+        return network.api_request(self.access_token, self.args.server,
             route, self.args.insecure, params)
-        
+
     def get_students(self, course):
         """Gets a list of students enrolled in the course"""
         response = self.request('course/{0}/get_students'.format(course),
