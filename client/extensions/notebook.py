eb4146e6b97677017e96da9b9f4195edcb1d3771
Alvin Wan <hi@alvinwan.com>
2015-07-19 17:57:05 -0700

    finished script extraction

--- client/extensions/notebook.py
+++ client/extensions/notebook.py
@@ -9,8 +9,18 @@ Extension for Jupyter Notebook
 """
 
 from . import Extension, extension
+import json
 import os
 import subprocess
+
+
+def shell(f):
+	""" Wraps all shell commands, prints command first """
+	def helper(*args, **kwargs):
+		print('[Notebook] %s' % str(args[1:]))
+		return f(*args, **kwargs)
+	helper.__name__ = f.__name__
+	return helper
 
 
 @extension
@@ -26,9 +36,10 @@ class NotebookExt(Extension):
 		""" setup dependencies, and cache code blocks """
 		if self.args.nb:
 			self.terminate_after_setup = True
+			self.extract_book(self.args.nb)
 		else:
 			self.setup_makefile()
-			self.call('make', 'all')
+			self.call('make', 'all', '--quiet')
 		return self
 	
 	def run(self, assign):
@@ -42,16 +53,22 @@ class NotebookExt(Extension):
 	# HELPER METHODS #
 	##################
 	
+	@shell
 	def call(self, *cmd):
 		""" Shell command """
-		print('[Notebook] %s' % str(cmd))
-		print(subprocess.call(cmd))
+		return subprocess.call(cmd)
+		
+	@shell
+	def return_output(self, *cmd):
+		""" Get output of shell command """
+		return subprocess.check_output(cmd)
 
 	def setup_makefile(self):
 		""" Setup makefile, mapping python files to notebooks """
 		print('[Notebook] Generating makefile dependencies.')
 		template = """\
 {name}.py: {name}.ipynb
+	@echo "[Notebook] Extracting {name}.py from {name}.ipynb"
 	ok --extension notebook --extargs '{args}'
 		"""
 		books = [f.split('.')[0] for f in os.listdir('.') if f.endswith('.ipynb')]
@@ -60,5 +77,19 @@ class NotebookExt(Extension):
 		content = 'all: %s\n\n' % ' '.join(all)
 		content += '\n'.join(deps)
 		open('makefile', 'w').write(content)
+		
+	def extract_book(self, name):
+		""" Extract python script from IPython source """
+		book = '%s.ipynb' % name
+		py = '%s.py' % name
+		cells = json.loads(open(book, 'r').read())['cells']
+		scripts = [self.extract_lines(cell['source']) 
+		           for cell in cells if cell['cell_type'] == 'code']
+		script = '\n'.join(scripts)
+		open(py, 'w').write(script)
+		
+	def extract_lines(self, lines):
+		""" Extracts code segment from IPython source block """
+		return '\n'.join([line for line in lines if line[0] != '%'])
 
 Extension = NotebookExt
