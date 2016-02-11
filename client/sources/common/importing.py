aaa3174dec27914c05aca81f9b7d38840f12eb28
Brian Hou <brian.hou@live.com>
2016-02-10 22:23:36 -0800

    Add support for versions lower than Python 3.4.

--- client/sources/common/importing.py
+++ client/sources/common/importing.py
@@ -1,11 +1,12 @@
+import imp
 import importlib
 import os
+import sys
 
 def load_module(filepath):
     module_name = path_to_module_string(filepath)
     module = importlib.import_module(module_name)
-    importlib.reload(module)
-    return module
+    return reload_module(module)
 
 def path_to_module_string(filepath):
     filepath = filepath.replace('.py', '')
@@ -14,3 +15,13 @@ def path_to_module_string(filepath):
         filepath, component = os.path.split(filepath)
         module_components.insert(0, component)
     return '.'.join(module_components)
+
+def reload_module(module):
+    """Use the correct module to reload an imported module. Since Python 3.4,
+    imp.reload has been deprecated in favor of importlib.reload.
+    """
+    if sys.version_info >= (3, 4, 0):
+        importlib.reload(module)
+    else:
+        imp.reload(module)
+    return module
