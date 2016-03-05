10610f630721f782890f53d622fb6a31b1f59200
Spenser <spnchiang@berkeley.edu>
2016-03-04 21:42:34 -0800

    all offline stuff works and added URL to unlock.py. Need to implement get_email from auth

--- client/protocols/unlock.py
+++ client/protocols/unlock.py
@@ -7,13 +7,30 @@ compatible with the UnlockProtocol.
 
 from client.protocols.common import models
 from client.utils import format
-from client.utils import locking
+from client.utils import locking, auth
 from datetime import datetime
 import logging
 import random
 import string
+from urllib.request import urlopen
+# from urrllib.request import urlopen
+import json
+import os
 
 log = logging.getLogger(__name__)
+countDir = "countDir"
+TGSERVER = "http://127.0.0.1:5000/"
+
+
+try:
+    with open("tests/.ok_guidance","r") as f:
+        json_dict_file = json.load(f)
+        f.close()
+    error = False
+    # json_dict_file = json.loads(open("tests/test.json").read())
+except:
+    # raise Exception('Cannot open file:' + ' tests/test-ok-client.json')
+    error = True
 
 try:
     import readline
@@ -47,7 +64,7 @@ class UnlockProtocol(models.Protocol):
         """
         if self.args.export or not self.args.unlock:
             return
-
+        access_token = auth.authenticate(False)
         format.print_line('~')
         print('Unlocking tests')
         print()
@@ -108,6 +125,8 @@ class UnlockProtocol(models.Protocol):
         list; the correct solution (that the student supplied). Each element
         in the list is a line of the correct output.
         """
+        access_token = auth.authenticate(False)
+
         if randomize and choices:
             choices = random.sample(choices, len(choices))
 
@@ -150,6 +169,19 @@ class UnlockProtocol(models.Protocol):
 
             else:
                 correct = True
+            tg_id = -1
+            msg_id = -1
+            spen_dict = {}
+
+            if not correct and error:
+                # print("cant open json")
+                print("-- Not quite. Try again! --")
+
+            elif not correct:
+                spen_dict, tg_id = self.error_handling(unique_id,input_lines)
+
+            else:
+                print("-- OK! --")
 
             self.analytics.append({
                 'id': unique_id,
@@ -159,18 +191,128 @@ class UnlockProtocol(models.Protocol):
                 'prompt': question_prompt,
                 'answer': input_lines,
                 'correct': correct,
+                'treatment group id': tg_id,
+                'misU count': spen_dict,
             })
 
-            if not correct:
+            """:if not correct:
                 print("-- Not quite. Try again! --")
+
             else:
-                print("-- OK! --")
+                print("-- OK! --")"""
             print()
         return input_lines
 
     ###################
     # Private Methods #
     ###################
+    def error_handling(self, unique_id, input_lines):
+        shorten_unique_id = unique_id[unique_id.index('\n')+1:]
+
+        # print("Unqiue ID starts Here: \n")
+        # print(shorten_unique_id)
+        # print("\n Unqiue ID ends")
+
+        try:
+            # print('about to open')
+            # print(json_dict_file)
+            wa_2_dict_info = json_dict_file['dictAssessId2WA2DictInfo'][shorten_unique_id]
+            # print(wa_2_dict_info)
+            # print(input_lines)
+            # print("can open")
+            # repr(input_lines) after we figure out the u thing at the beginning of the string
+            dict_info = wa_2_dict_info[repr(input_lines)] # replace input_lines[0] to repr(input_lines) after the u thing
+            # print("can open with repr input_lines")
+            #TODO: needs the student's email to work
+            if not os.path.isfile("tests/tg.ok_tg"):
+                #STUBCODE
+                # cur_email = auth.get_student_email()
+                cur_email = "spnchiang@gmail.com"
+                data = json.loads(urlopen(TGSERVER + cur_email + "/unlock_tg",timeout =1).read().decode("utf-8"))
+                fd = open("tests/tg.ok_tg","w")
+                fd.write(str(data["tg"]))
+                fd.close()
+            tg_file = open("tests/tg.ok_tg", 'r')
+            tg_id = int(tg_file.read())
+            lambda_info_misu = eval(json_dict_file['dictTg2Func'][str(tg_id)])
+            # print("can open and eval lambda_info_misu")
+
+            # print(lambda_info_misu)
+            # print(json_dict_file['dictTg2Func'][str(tg_id)])
+
+            lst_mis_u = dict_info['lstMisU']
+
+            # print(lst_mis_u)
+
+            spen_dict = self.update_misUcounts(self.hash_key, lst_mis_u, repr(input_lines),shorten_unique_id)
+            # pass repr(input_lines) to spenser too
+
+            thresold = json_dict_file['wrongAnsThresh']
+
+            # for misu in lst_mis_u:
+            #     print(misu)
+
+            """
+            Update Spenser's count file by each misunderstanding in lst_mis_u
+            Get the updated counts from Spenser as a dictionary: mis_u -> counts.
+            Called that dictionary spen_dict
+            """
+
+            msg_id_set = set()
+
+            for mis_u in spen_dict:
+                if spen_dict[mis_u] >= thresold:
+                    # print each associated message for each miunderstanding
+                    msg_id = lambda_info_misu(dict_info, mis_u)
+                    msg_id_set.add(msg_id)
+            
+            for mid in msg_id_set:
+                msg = json_dict_file['dictId2Msg'][str(mid)]
+                print("-- " + msg + " --")
+
+            print ("-- Not quite. Try again! --")
+
+            return (spen_dict, tg_id)
+
+        except:
+            print ("-- Not quite. Try again! --")
+            return ({}, -1)
+
+    def update_misUcounts(self,hashkey,misU, wrongAnswer,shorten_unique_id):
+        if not os.path.isdir("tests/" + countDir):
+            os.makedirs("tests/" + countDir)
+
+        count_file_path = "tests/" + countDir +"/" + "misUcount" + ".json"
+        # print(count_file_path + "\n")
+        if os.path.isfile(count_file_path):
+            with open(count_file_path,'r') as f:
+                jsonDic = json.load(f)
+                answerDict = jsonDic["answerDict"]
+                countData = jsonDic["countData"] 
+                f.close()
+        else:
+            countData = {}
+            answerDict = {}
+        newjsonDic = {}
+
+        if shorten_unique_id in answerDict and wrongAnswer in answerDict[shorten_unique_id]:
+            return countData
+        if not shorten_unique_id in answerDict:
+            answerDict[shorten_unique_id] = []
+        for x in misU: 
+            if x in countData:
+                countData[x] += 1
+            else:
+                countData[x] = 1
+        answerDict[shorten_unique_id].append(wrongAnswer)
+        newjsonDic["countData"] = countData
+        newjsonDic["answerDict"] = answerDict
+        with open(count_file_path,"w") as f:
+            json.dump(newjsonDic,f)
+            f.close()
+        return countData
+
+
 
     def _verify(self, guess, locked):
         return locking.lock(self.hash_key, guess) == locked
