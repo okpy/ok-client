002dafac0836a183ea90bf68d0dc22460d659d48
Colin Schoen <cschoen@berkeley.edu>
2016-08-31 16:29:11 -0700

    Warn and exit on 32 bit installations of Python (#216)
    * Warn and exit on 32 bit installations of Python
    
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>
    
    * Update warning/error message
    
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>
    
    * Change --
    
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>
    
    * Add back --flag and fix string concatenation
    
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- client/cli/ok.py
+++ client/cli/ok.py
@@ -157,6 +157,11 @@ def main():
     # Checking user's Python bit version
     bit_v = (8 * struct.calcsize("P"))
     log.debug("Python {}bit".format(bit_v))
+    if not args.local and bit_v == 32:
+        #If they aren't running --local and on 32 warn and quit
+        print('We cannot authenticate you with a 32 bit version of Python. To '
+               'run locally use the --local flag')
+        exit(0)
 
     if args.version:
         print("okpy=={}".format(client.__version__))
