3641ce0808f3551e8fc3ef31048290354675a9f1
Kyle Raftogianis <knrafto@gmail.com>
2016-12-26 16:03:17 -0800

    Remove the restore protocol (#252)

--- client/protocols/file_contents.py
+++ client/protocols/file_contents.py
@@ -18,9 +18,6 @@ class FileContentsProtocol(models.Protocol):
         RETURNS:
         dict; a mapping of source filepath -> contents as strings.
         """
-        if self.args.restore:
-            return
-
         files = {}
         # TODO(albert): move this to AnalyticsProtocol
         if self.args.submit:
