0d78b112f55595315abf08e66319282f62ef641e
Sumukh Sridhara <sumukh1@gmail.com>
2017-01-23 12:36:09 -0800

    Support scoring protocol
    Remove verbose arg

--- client/api/notebook.py
+++ client/api/notebook.py
@@ -5,21 +5,37 @@ class Notebook:
     def __init__(self, filepath=None, cmd_args=None):
         self.assignment = load_assignment(filepath, cmd_args)
 
+    def run(self, protocol, messages, **kwargs):
+        if protocol not in self.assignment.protocol_map:
+            print("{} has not been included in the .ok config".format(protocol))
+            return
+        return self.assignment.protocol_map[protocol].run(messages, **kwargs)
+
     def auth(self, force=False, inline=True):
-        self.assignment.cmd_args.set_args(['--no-browser'])
+        if inline:
+            self.assignment.cmd_args.set_args(['--no-browser'])
         ok_auth.authenticate(self.assignment, force=force)
 
     def grade(self, *args, **kwargs):
         return self.assignment.grade(*args, **kwargs)
 
+    def score(self, env=None):
+        messages = {}
+        self.assignment.cmd_args.set_args(['--score'])
+        if env is None:
+            import __main__
+            env = __main__.__dict__
+        self.run('scoring', messages, env=env)
+        return messages
+
     def backup(self):
         messages = {}
         self.assignment.cmd_args.set_args(['--backup'])
-        self.assignment.protocol_map['file_contents'].run(messages)
-        return self.assignment.protocol_map['backup'].run(messages)
+        self.run('file_contents', messages)
+        return self.run('backup', messages)
 
     def submit(self):
         messages = {}
         self.assignment.cmd_args.set_args(['--submit'])
-        self.assignment.protocol_map['file_contents'].run(messages)
-        return self.assignment.protocol_map['backup'].run(messages)
+        self.run('file_contents', messages)
+        return self.run('backup', messages)
