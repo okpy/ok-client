934cdb35a6a972700bc01c6fc0ed21b78e6411ba
Eric Pai <ericpai@berkeley.edu>
2017-06-21 00:52:31 -0700

    add tests for no cooldown when unlocking

--- tests/protocols/rate_limit_test.py
+++ tests/protocols/rate_limit_test.py
@@ -19,12 +19,15 @@ class RateLimitProtocolTest(unittest.TestCase):
         self.assignment = mock.Mock(spec=Assignment)
         self.assignment.src = ['foo.py', 'bar.py']
         self.proto = rate_limit.protocol(self.cmd_args, self.assignment, cooldown=(0,0,2,4))
+        self.test = mock.Mock(spec=models.Test)
+        self.test.name = 'test'
 
     def callRun(self):
         messages = {}
         self.proto.run(messages)
-        self.assertIn('rate_limit', messages)
-        return messages['rate_limit']
+        if not self.cmd_args.unlock and not self.cmd_args.unlock:
+            self.assertIn('rate_limit', messages)
+            return messages['rate_limit']
 
     def make_attempt(self, test, attempts, succeeds=True):
         self.assignment.specified_tests = [test]
@@ -33,34 +36,42 @@ class RateLimitProtocolTest(unittest.TestCase):
                 self.callRun()
         else:
             self.callRun()
-        self.assertTrue(rate_limit.get(test.name, 'attempts') == attempts, 'attempts not correct')
+        self.assertTrue(storage.get(test.name, 'attempts', 0) == attempts, 'attempts not correct')
 
     def testManyAttempts(self):
-        test = mock.Mock(spec=models.Test)
-        test.name = 'test'
-        self.make_attempt(test, 1)
-        self.make_attempt(test, 2, succeeds=True)
-        self.make_attempt(test, 2, succeeds=False)
+        self.make_attempt(self.test, 1)
+        self.make_attempt(self.test, 2, succeeds=True)
+        self.make_attempt(self.test, 2, succeeds=False)
         time.sleep(2)
-        self.make_attempt(test, 3, succeeds=True)
-        self.make_attempt(test, 3, succeeds=False)
+        self.make_attempt(self.test, 3, succeeds=True)
+        self.make_attempt(self.test, 3, succeeds=False)
         time.sleep(2)
-        self.make_attempt(test, 3, succeeds=False)
+        self.make_attempt(self.test, 3, succeeds=False)
         time.sleep(2)
-        self.make_attempt(test, 4, succeeds=True)
+        self.make_attempt(self.test, 4, succeeds=True)
         time.sleep(4)
-        self.make_attempt(test, 5, succeeds=True)
+        self.make_attempt(self.test, 5, succeeds=True)
 
     def testSuppressOnCorrect(self):
-        test = mock.Mock(spec=models.Test)
-        test.name = 'test'
-        self.make_attempt(test, 1)
-        self.make_attempt(test, 2, succeeds=True)
-        storage.store(test.name, 'correct', True)
+        self.make_attempt(self.test, 1)
+        self.make_attempt(self.test, 2, succeeds=True)
+        storage.store(self.test.name, 'correct', True)
         # attempts on a correct question shouldn't increase counter
-        self.make_attempt(test, 2, succeeds=True)
-        self.make_attempt(test, 2, succeeds=True)
-        storage.store(test.name, 'correct', False)
-        self.make_attempt(test, 2, succeeds=False)
+        self.make_attempt(self.test, 2, succeeds=True)
+        self.make_attempt(self.test, 2, succeeds=True)
+        storage.store(self.test.name, 'correct', False)
+        self.make_attempt(self.test, 2, succeeds=False)
         time.sleep(2)
-        self.make_attempt(test, 3, succeeds=True)
+        self.make_attempt(self.test, 3, succeeds=True)
+
+    def testSuppressOnUnlock(self):
+        # set unlock (-u)
+        self.cmd_args.unlock = True
+        self.proto = rate_limit.protocol(self.cmd_args, self.assignment, cooldown=(0,0,2,4))
+        # successive attempts should not cooldown
+        # attempts not tracked when unlocking
+        self.make_attempt(self.test, 0)
+        self.make_attempt(self.test, 0, succeeds=True)
+        self.make_attempt(self.test, 0, succeeds=True)
+        self.make_attempt(self.test, 0, succeeds=True)
+
