f543287e9bcc01daa1c039a029f71d315328b9f3
Eric Pai <ericpai@berkeley.edu>
2017-07-07 16:12:28 -0700

    add logging to exceptions

--- client/exceptions.py
+++ client/exceptions.py
@@ -1,8 +1,20 @@
 """Client exceptions."""
 
+import client
 
-class OkException(BaseException):
+import sys
+import logging
+
+log = logging.getLogger(__name__)   # Get top-level logger
+
+class OkException(Exception):
     """Base exception class for OK."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        log.debug('Exception raised: {}'.format(type(self).__name__))
+        log.debug('python version: {}'.format(sys.version_info))
+        log.debug('okpy version: {}'.format(client.__version__))
+
 
 
 class AuthenticationException(OkException):
