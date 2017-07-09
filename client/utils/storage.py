1057b53861efdb4fb377bd47ed8925c445663129
Eric Pai <ericpai@berkeley.edu>
2017-07-09 01:17:38 -0700

    reimplement store as MutableMapping

--- client/utils/storage.py
+++ client/utils/storage.py
@@ -1,35 +1,78 @@
 import shelve # persistance
 import hmac # security
+import os
+from contextlib import contextmanager
+from collections.abc import MutableMapping
+
+from client.exceptions import ProtocolException
+from client.utils.config import SHELVE_FILE, SECURITY_KEY
+
+__all__ = ['get_store']
 
 ##################
 # Secure Storage #
 ##################
 
-SHELVE_FILE = '.ok_storage'
-SECURITY_KEY = 'uMWm4sviPK3LyPzgWYFn'.encode('utf-8')
+TAMPER_MESSAGE = \
+"""
+Integrity Error: {} was tampered.
+Please reverse the changes, or redownload the assignment.
+"""
 
 def mac(value):
     mac = hmac.new(SECURITY_KEY)
     mac.update(repr(value).encode('utf-8'))
     return mac.hexdigest()
 
-def contains(root, key):
-    key = '{}-{}'.format(root, key)
-    with shelve.open(SHELVE_FILE) as db:
-        return key in db
+class SecureStore(MutableMapping):
 
-def store(root, key, value):
-    key = '{}-{}'.format(root, key)
-    with shelve.open(SHELVE_FILE) as db:
-        db[key] = {'value': value, 'mac': mac(value)}
-    return value
+    def __init__(self, file, prefix):
+        self._file = file
+        self._prefix = prefix
 
-def get(root, key, default=None):
-    if not contains(root, key):
-        return default
-    key = '{}-{}'.format(root, key)
-    with shelve.open(SHELVE_FILE) as db:
-        data = db[key]
-        if not hmac.compare_digest(data['mac'], mac(data['value'])):
-            raise ProtocolException('{} was tampered.  Reverse changes, or redownload assignment'.format(SHELVE_FILE))
-    return data['value']
+    @contextmanager
+    def open(self):
+        with shelve.open(self._file) as db:
+            yield db
+
+    def format_key(self, key):
+        return os.path.join(self._prefix, key)
+
+    def __getitem__(self, key):
+        key = self.format_key(key)
+        with self.open() as db:
+            data = db[key]
+            if not hmac.compare_digest(data['mac'], mac(data['value'])):
+                raise ProtocolException(TAMPER_MESSAGE.format(self._file))
+            return data['value']
+
+    def __setitem__(self, key, value):
+        key = self.format_key(key)
+        with self.open() as db:
+            db[key] = {'value': value, 'mac': mac(value)}
+
+    def __delitem__(self, key):
+        key = self.format_key(key)
+        with self.open() as db:
+            del db[key]
+
+    def __iter__(self):
+        with self.open() as db:
+            keys = tuple(db.keys())
+        return iter(keys)
+
+    def __len__(self):
+        with self.open() as db:
+            return len(db)
+
+    def clear(self):
+        for key in self:
+            if key.startswith(self._prefix):
+                with self.open() as db:
+                    del db[key]
+
+def get_store(*prefixes):
+    prefix = os.path.join(*prefixes)
+    return SecureStore(SHELVE_FILE, prefix)
+
+
