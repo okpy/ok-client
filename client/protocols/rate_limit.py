e022b597c58f732a0a06619429f9acb01fd63114
Eric Pai <ericpai@berkeley.edu>
2017-07-09 01:18:39 -0700

    refactor rate_limit.py to use new store interface

--- client/protocols/rate_limit.py
+++ client/protocols/rate_limit.py
@@ -1,8 +1,8 @@
 from client.protocols.common import models
 from client.exceptions import EarlyExit
-from client.utils.storage import contains, get, store
+from client.utils.storage import get_store
+
 import time
-import textwrap
 
 
 COOLDOWN_POLICY = (0, 60,) # uniform 60s cooldown
@@ -36,28 +36,37 @@ class RateLimitProtocol(models.Protocol):
         if self.args.score or self.args.unlock:
             return
         analytics = {}
-        tests = self.assignment.specified_tests
-        for test in tests:
-            if get(test.name, 'correct', default=False):
-                continue # suppress rate limiting if question is correct
-            last_attempt, attempts = self.check_attempt(test)
-            analytics[test.name] = {
-                'attempts': store(test.name, 'attempts', attempts),
-                'last_attempt': store(test.name, 'last_attempt', last_attempt)}
-
+        for test in self.assignment.specified_tests:
+            store = get_store(self.assignment.name, test.name)
+            # suppress rate limiting if question is correct
+            if store.get('correct', False):
+                continue
+            analytics[test.name] = self.check_attempt(test)
         messages['rate_limit'] = analytics
 
+    def get_cooldown(self, attempts):
+        try:
+            return self.cooldown[attempts]
+        except IndexError:
+            # Repeat the last cooldown in the cooldown policy
+            return self.cooldown[-1]
+
     def check_attempt(self, test):
+        store = get_store(self.assignment.name, test.name)
         now = int(time.time())
-        last_attempt = get(test.name, 'last_attempt', now)
-        attempts = get(test.name, 'attempts', 0)
-        secs_elapsed = now - last_attempt
-        cooldown_time = self.cooldown[attempts] if attempts < len(self.cooldown) else self.cooldown[-1]
-        cooldown = cooldown_time - secs_elapsed
+        last_attempt = store.get('last_attempt', now)
+        attempts = store.get('attempts', 0)
+        cooldown = self.get_cooldown(attempts) - (now - last_attempt)
         if attempts and cooldown > 0:
             files = ' '.join(self.assignment.src)
             raise EarlyExit(COOLDOWN_MSG.format(
-                wait=cooldown, question=test.name.lower(), tries=attempts, files=files))
-        return now, attempts + 1
+                wait=cooldown, question=test.name.lower(),
+                tries=attempts, files=files))
+        attempts += 1
+        last_attempt = now
+        store['attempts'] = attempts
+        store['last_attempt'] = last_attempt
+        return {'attempts': attempts, 'last_attempt': last_attempt}
+
 
 protocol = RateLimitProtocol
