e088ffc4d00af0c8585f3b9fa1538678843f3e97
Eric Pai <ericpai@berkeley.edu>
2017-07-09 01:21:57 -0700

    update rate_limit_test.py to use new store

--- tests/protocols/rate_limit_test.py
+++ tests/protocols/rate_limit_test.py
@@ -2,7 +2,8 @@
 from client.protocols import rate_limit
 from client.sources.common import models
 from client.exceptions import EarlyExit
-from client.utils import storage
+from client.utils.storage import get_store
+from client.utils.config import SHELVE_FILE
 from client.api.assignment import Assignment
 import mock
 import time
@@ -11,16 +12,23 @@ import unittest
 
 class RateLimitProtocolTest(unittest.TestCase):
     def setUp(self):
-        os.remove('.ok_storage') if os.path.exists('.ok_storage') else None
         self.cmd_args = mock.Mock()
         self.cmd_args.score = False
         self.cmd_args.unlock = False
         self.cmd_args.restore = False
         self.assignment = mock.Mock(spec=Assignment)
         self.assignment.src = ['foo.py', 'bar.py']
-        self.proto = rate_limit.protocol(self.cmd_args, self.assignment, cooldown=(0,0,2,4))
+        self.assignment.name = 'ratelimittest'
+        self.proto = rate_limit.protocol(self.cmd_args, self.assignment, cooldown=(0,0.5))
         self.test = mock.Mock(spec=models.Test)
         self.test.name = 'test'
+        self.assignment.specified_tests = [self.test]
+
+        self.store = get_store(self.assignment.name, self.test.name)
+        self.store.clear()
+
+    def tearDown(self):
+        get_store('ratelimittest').clear()
 
     def callRun(self):
         messages = {}
@@ -29,40 +37,36 @@ class RateLimitProtocolTest(unittest.TestCase):
             self.assertIn('rate_limit', messages)
             return messages['rate_limit']
 
-    def make_attempt(self, test, attempts, succeeds=True):
-        self.assignment.specified_tests = [test]
+    def make_attempt(self, attempts, succeeds=True):
         if not succeeds:
             with self.assertRaises(EarlyExit):
                 self.callRun()
         else:
-            self.callRun()
-        self.assertTrue(storage.get(test.name, 'attempts', 0) == attempts, 'attempts not correct')
+            try:
+                self.callRun()
+            except EarlyExit:
+                raise Exception('Exited Early when not supposed to!')
+        self.assertTrue(self.store.get('attempts', 0) == attempts, 'attempts not correct')
 
     def testManyAttempts(self):
-        self.make_attempt(self.test, 1)
-        self.make_attempt(self.test, 2, succeeds=True)
-        self.make_attempt(self.test, 2, succeeds=False)
-        time.sleep(2)
-        self.make_attempt(self.test, 3, succeeds=True)
-        self.make_attempt(self.test, 3, succeeds=False)
-        time.sleep(2)
-        self.make_attempt(self.test, 3, succeeds=False)
-        time.sleep(2)
-        self.make_attempt(self.test, 4, succeeds=True)
-        time.sleep(4)
-        self.make_attempt(self.test, 5, succeeds=True)
+        self.make_attempt(1)
+        self.make_attempt(1, succeeds=False)
+        time.sleep(1)
+        self.make_attempt(2)
+        self.make_attempt(2, succeeds=False)
+        time.sleep(1)
+        self.make_attempt(3)
 
     def testSuppressOnCorrect(self):
-        self.make_attempt(self.test, 1)
-        self.make_attempt(self.test, 2, succeeds=True)
-        storage.store(self.test.name, 'correct', True)
+        self.make_attempt(1)
+        self.store['correct'] = True
         # attempts on a correct question shouldn't increase counter
-        self.make_attempt(self.test, 2, succeeds=True)
-        self.make_attempt(self.test, 2, succeeds=True)
-        storage.store(self.test.name, 'correct', False)
-        self.make_attempt(self.test, 2, succeeds=False)
-        time.sleep(2)
-        self.make_attempt(self.test, 3, succeeds=True)
+        self.make_attempt(1)
+        self.make_attempt(1)
+        self.store['correct'] = False
+        self.make_attempt(1, succeeds=False)
+        time.sleep(1)
+        self.make_attempt(2)
 
     def testSuppressOnUnlock(self):
         # set unlock (-u)
@@ -70,8 +74,8 @@ class RateLimitProtocolTest(unittest.TestCase):
         self.proto = rate_limit.protocol(self.cmd_args, self.assignment, cooldown=(0,0,2,4))
         # successive attempts should not cooldown
         # attempts not tracked when unlocking
-        self.make_attempt(self.test, 0)
-        self.make_attempt(self.test, 0, succeeds=True)
-        self.make_attempt(self.test, 0, succeeds=True)
-        self.make_attempt(self.test, 0, succeeds=True)
+        self.make_attempt(0)
+        self.make_attempt(0)
+        self.make_attempt(0)
+        self.make_attempt(0)
 
