3428470b9202d399f8e5466d139d8ce0c56d517a
Colin Schoen <cschoen@berkeley.edu>
2018-01-30 20:46:30 -0800

    Use os.remove, os.rename instead of os.replace for Windows (#340)
    * Use os.remove, os.rename instead of os.replace
    
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>
    
    * Try to use os.remove, but fall back to os.remove and os.rename when on
    Windows
    
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>
    
    * Add back atomic comment regarding os.replace on POSIX
    
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- client/sources/ok_test/models.py
+++ client/sources/ok_test/models.py
@@ -179,8 +179,15 @@ class OkTest(models.Test):
         with open(test_tmp, 'w', encoding='utf-8') as f:
             f.write('test = {}\n'.format(json))
 
-        # Use an atomic rename operation to prevent test corruption
-        os.replace(test_tmp, self.file)
+        # Try to use os.replace, but if on Windows manually remove then rename
+        # (ref issue #339)
+        if os.name == 'nt':
+        # TODO(colin) Add additional error handling in case process gets killed mid remove/rename
+            os.remove(self.file)
+            os.rename(test_tmp, self.file)
+        else:
+            # Use an atomic rename operation to prevent test corruption
+            os.replace(test_tmp, self.file)
 
     @property
     def unique_id_prefix(self):
