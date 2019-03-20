b50a4986bb3fb90710b2da4769bbebefec58ff73
Ja (Thanakul) Wattanawong <ja@jwattanawong.com>
2019-03-20 16:24:42 -0700

    Improved clarity for submission error when wrong file is saved (#369)
    * improved saving error clarity
    
    * replace file name in string

--- client/api/notebook.py
+++ client/api/notebook.py
@@ -68,11 +68,19 @@ class Notebook:
     def submit(self):
         messages = {}
         self.assignment.set_args(submit=True)
-        self.save(messages)
-        return self.run('backup', messages)
-
+        if self.save(messages):
+            return self.run('backup', messages)
+        else:
+            filename = self.assignment.src[0]
+            print("Making a best attempt to submit latest \'{}\',"
+                " last modified at {}".format(filename, time.ctime(os.path.getmtime(filename))))
+            return self.run('backup', messages)
+        
     def save(self, messages, delay=0.5, attempts=3):
-        self.save_notebook()
+        saved = self.save_notebook()
+        if not saved:
+            return None
+
         for _ in range(attempts):
             self.run('file_contents', messages)
             if validate_contents(messages['file_contents']):
@@ -101,6 +109,7 @@ class Notebook:
             display(Javascript('document.querySelector(\'[data-command="docmanager:save"]\').click();'))   
                        
         print('Saving notebook...', end=' ')
+
         ipynbs = [path for path in self.assignment.src
                   if os.path.splitext(path)[1] == '.ipynb']
         # Wait for first .ipynb to save
@@ -109,10 +118,13 @@ class Notebook:
                 print("Saved '{}'.".format(ipynbs[0]))
             else:
                 log.warning("Timed out waiting for IPython save")
-                print("Could not save your notebook. Make sure your notebook"
-                      " is saved before sending it to OK!")
+                print("Could not automatically save \'{}\'".format(ipynbs[0]))
+                print("Make sure your notebook"
+                      " is correctly named and saved before submitting to OK!".format(ipynbs[0]))
+                return False                
         else:
-            print()
+            print("No valid file sources found")
+        return True
 
 def login_with_env(assignment):
     access_token = os.environ.get('OKPY_ACCESS_TOKEN')
