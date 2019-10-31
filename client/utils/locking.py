8bdf6b99a444289f79fd8910cda6b8dfa34d1060
Kavi Gupta <kavi@globalmbox.com>
2019-10-31 03:32:42 -0700

    explicitly call digestmod=md5 to make it work on 38

--- client/utils/locking.py
+++ client/utils/locking.py
@@ -4,4 +4,4 @@ import hmac
 
 def lock(key, text):
     """Locks the given text using the given key and returns the result"""
-    return hmac.new(key.encode('utf-8'), text.encode('utf-8')).hexdigest()
+    return hmac.new(key.encode('utf-8'), text.encode('utf-8'), digestmod='md5').hexdigest()
