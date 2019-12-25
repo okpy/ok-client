979aff147346e6f0daea57bd9797c26ac1d5fdf8
Kavi Gupta <kavi@globalmbox.com>
2019-12-24 23:07:30 -0800

    make tests a bit more sophisticated

--- tests/end_to_end/smoke_test.py
+++ tests/end_to_end/smoke_test.py
@@ -5,7 +5,24 @@ import subprocess
 
 class SmokeTest(unittest.TestCase):
 
-    def testRuns(self):
-        with tempfile.TemporaryDirectory() as f:
-            publish.package_client(f)
-            subprocess.check_call(['python', 'ok', '--version'], cwd=f, stdout=None)
+    def setUp(self):
+        self.directory = tempfile.TemporaryDirectory().name
+        publish.package_client(self.directory)
+
+    def run_ok(self, *args):
+        proc = subprocess.Popen(
+            ['python', 'ok', *args],
+            cwd=self.directory,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        return proc.stdout.read().decode("utf-8"), proc.stderr.read().decode("utf-8")
+
+    def testVersion(self):
+        stdout, stderr = self.run_ok("--version")
+        self.assertRegex(stdout, "^okpy==.*")
+        self.assertEqual(stderr, "")
+
+    def testUpdate(self):
+        stdout, stderr = self.run_ok("--update")
+        self.assertRegex(stdout, "Current version: v[0-9.]+\nChecking for software updates...\nOK is up to date")
+        self.assertEqual(stderr, "")
