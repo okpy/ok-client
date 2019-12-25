6db65b1a0347f9b6c097b20f87f692d9d4bf0d40
Kavi Gupta <kavi@globalmbox.com>
2019-12-24 22:08:28 -0800

    remove request patching, which I think is now unnecssary?

--- client/utils/config.py
+++ client/utils/config.py
@@ -2,7 +2,6 @@ import os
 
 CONFIG_DIRECTORY = os.path.join(os.path.expanduser('~'), '.config', 'ok')
 REFRESH_FILE = os.path.join(CONFIG_DIRECTORY, "auth_refresh")
-CERT_FILE = os.path.join(CONFIG_DIRECTORY, "cacert.pem")
 
 def create_config_directory():
     if not os.path.exists(CONFIG_DIRECTORY):
