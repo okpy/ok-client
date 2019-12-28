e79234bf4b7db8e4e875f54d8367c3f4daa6858b
Kavi Gupta <kavi@globalmbox.com>
2019-12-27 16:31:55 -0800

    print with success and failure

---
+++ client/utils/printer.py
@@ -0,0 +1,8 @@
+
+from colorama import Fore, Style
+
+def print_error(text):
+    print(Fore.RED + text + Style.RESET_ALL)
+
+def print_success(text):
+    print(Fore.GREEN + text + Style.RESET_ALL)
