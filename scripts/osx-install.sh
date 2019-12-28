0d96f5f50cc87b882ea0073d02343672660b5074
Mehrdad Niknami <>
2019-12-28 02:51:14 -0800

    Auto-detect latest minor version of Python for Mac OS X installations

--- scripts/osx-install.sh
+++ scripts/osx-install.sh
@@ -1,5 +1,10 @@
 brew install pyenv
-pyenv install $PYV
-pip3 install virtualenv
-virtualenv -p python3 ~/venv
+py_full_version="${TRAVIS_PYTHON_VERSION}"
+if [ -n "${py_full_version##*.*.*}" ]; then  # Do we have the full version number?
+	pyenv install "$[py_full_version}"
+else
+	git clone --depth=1 "https://github.com/momo-lab/xxenv-latest.git" "$(pyenv root)/plugins/xxenv-latest"
+	pyenv install-latest "${py_full_version}"
+fi
+python"${py_full_version%%.*}" -m venv ~/venv
 . ~/venv/bin/activate
