0486dc705f20dfffca77ab6d3927c0b075827922
Kavi Gupta <kavi@berkeley.edu>
2020-04-30 03:26:11 -0700

    Add encryption module to be used for the ok-exam tool

--- client/cli/publish.py
+++ client/cli/publish.py
@@ -14,7 +14,8 @@ CONFIG_NAME = 'config.ok'
 EXTRA_PACKAGES = [
     'requests', 'certifi', 'urllib3', 'chardet', 'idna', # requests/certifi and recursive deps
     'coverage', # coverage and recursive deps
-    'pytutor', 'ast_scope', 'attr' # pytutor and recursive deps
+    'pytutor', 'ast_scope', 'attr', # pytutor and recursive deps
+    'cryptography'
 ]
 
 def abort(message):
