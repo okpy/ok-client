4bb121f3746badbecaf838f40f9c16e7c58dccc2
Kavi Gupta <kavi@berkeley.edu>
2020-05-01 02:28:13 -0700

    fix regex

--- tests/end_to_end/smoke_test.py
+++ tests/end_to_end/smoke_test.py
@@ -53,7 +53,7 @@ class SmokeTest(unittest.TestCase):
     def testUpdate(self):
         stdout, stderr = self.run_ok("--update")
         self.assertEqual(stderr, "")
-        self.assertRegex(stdout, "Current version: v[0-9.]+\nChecking for software updates...\nOK is up to date")
+        self.assertRegex(stdout, "Current version: v[0-9.]+\n.*OK is up to date.*")
 
     def testRunNoArgument(self):
         self.add_file("test.ok", json.dumps(
