668212eefd8d69be735b0daa067603a232812dd7
Kavi Gupta <kavi@berkeley.edu>
2020-05-09 01:56:19 -0700

    Add memory limit, defaulted to 1GB

--- tests/end_to_end/smoke_test.py
+++ tests/end_to_end/smoke_test.py
@@ -2,20 +2,18 @@ import json
 
 from tests.end_to_end.end_to_end_test import EndToEndTest
 
+TOO_MUCH_MEMORY_TEST = """
+def f():
+    '''
+    >>> f()
+    2
+    '''
+    return 0 * len(str(list(range(10 ** 7)))) + 2
+"""
 
 class SmokeTest(EndToEndTest):
 
-    def testVersion(self):
-        stdout, stderr = self.run_ok("--version")
-        self.assertEqual(stderr, "")
-        self.assertRegex(stdout, "^okpy==.*")
-
-    def testUpdate(self):
-        stdout, stderr = self.run_ok("--update")
-        self.assertEqual(stderr, "")
-        self.assertRegex(stdout, "Current version: v[0-9.]+\nOK is up to date")
-
-    def testRunNoArgument(self):
+    def add_test_ok(self):
         self.add_file("test.ok", json.dumps(
             {
                 "name": "Test Assignment",
@@ -37,6 +35,30 @@ class SmokeTest(EndToEndTest):
                 ]
             }
         ))
+
+    def testVersion(self):
+        stdout, stderr = self.run_ok("--version")
+        self.assertEqual(stderr, "")
+        self.assertRegex(stdout, "^okpy==.*")
+
+    def testUpdate(self):
+        stdout, stderr = self.run_ok("--update")
+        self.assertEqual(stderr, "")
+        self.assertRegex(stdout, "Current version: v[0-9.]+\nOK is up to date")
+
+    def testRunNoArgument(self):
+        self.add_test_ok()
         stdout, stderr = self.run_ok("--local")
         self.assertEqual(stderr, "")
         self.assertRegex(stdout, "0 test cases passed! No cases failed")
+
+    def testRunTooMuchMemory(self):
+        self.add_test_ok()
+        self.add_file("test.py", TOO_MUCH_MEMORY_TEST)
+        stdout, stderr = self.run_ok("--local", "-q", "f", "--max-memory", "100")
+        self.assertEqual(stderr, "")
+        self.assertRegex(stdout, "MemoryError")
+        self.assertRegex(stdout, "0 test cases passed before encountering first failed test case")
+        stdout, stderr = self.run_ok("--local", "-q", "f", "--max-memory", "1000")
+        self.assertEqual(stderr, "")
+        self.assertRegex(stdout, "1 test cases passed! No cases failed")
