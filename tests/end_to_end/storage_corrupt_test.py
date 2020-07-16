3e74bbc7994e4d0dac31dbc99ea6cf7253e0a29d
Kavi Gupta <kavi@berkeley.edu>
2020-07-15 17:19:39 -0700

    add test

---
+++ tests/end_to_end/storage_corrupt_test.py
@@ -0,0 +1,17 @@
+import json
+import tempfile
+
+from tests.end_to_end.end_to_end_test import EndToEndTest
+
+
+class EncryptionTest(EndToEndTest):
+    def testEncrypt(self):
+        self.copy_examples()
+        stdout, stderr = self.run_ok("-q", "q1")
+        self.assertEqual(stderr, "")
+        # mess up the shelve
+        self.add_file(".ok_storage.dir", "\0")
+        self.add_file(".ok_storage.bak", "\0")
+        self.add_file(".ok_storage.dat", "\0")
+        stdout, stderr = self.run_ok("-q", "q1")
+        self.assertEqual(stderr, "")
