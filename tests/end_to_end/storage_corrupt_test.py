f0c3faa89727b7100c5795d668bbcef2f6c370f4
Kavi Gupta <kavi@berkeley.edu>
2020-07-15 18:05:56 -0700

    fix test

--- tests/end_to_end/storage_corrupt_test.py
+++ tests/end_to_end/storage_corrupt_test.py
@@ -8,10 +8,10 @@ class EncryptionTest(EndToEndTest):
     def testEncrypt(self):
         self.copy_examples()
         stdout, stderr = self.run_ok("-q", "q1")
-        self.assertEqual(stderr, "")
+        self.assertOnlyInvalidGrant(stderr)
         # mess up the shelve
         self.add_file(".ok_storage.dir", "\0")
         self.add_file(".ok_storage.bak", "\0")
         self.add_file(".ok_storage.dat", "\0")
         stdout, stderr = self.run_ok("-q", "q1")
-        self.assertEqual(stderr, "")
+        self.assertOnlyInvalidGrant(stderr)
